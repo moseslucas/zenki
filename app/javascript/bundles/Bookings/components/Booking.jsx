11e287a9953fcaeff4a0ff12547c62cbb9f6b31f
moseslucas <mo@moseslucas.com>
2017-12-27 20:30:43 +0800

    Added react-google-maps to /bookings

--- app/javascript/bundles/Bookings/components/Booking.jsx
+++ app/javascript/bundles/Bookings/components/Booking.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker  } from "react-google-maps"
 
 export default class Booking extends React.Component {
   constructor(props) {
@@ -11,8 +12,24 @@ export default class Booking extends React.Component {
     const {title} = this.state
     return (
       <div>
-        <h1>{title}</h1>
+        <MyMapComponent
+          isMarkerShown
+          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `400px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
       </div>
     );
   }
 }
+
+
+const MyMapComponent = withScriptjs(withGoogleMap((props) =>
+  <GoogleMap
+    defaultZoom={8}
+    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+  >
+    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+  </GoogleMap>
+))
