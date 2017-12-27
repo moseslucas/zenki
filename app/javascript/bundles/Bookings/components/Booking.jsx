bb8a12b4bfb2c34ed53aaf5b81292b81b343d301
moseslucas <mo@moseslucas.com>
2017-12-27 20:46:17 +0800

    Added google maps api key

--- app/javascript/bundles/Bookings/components/Booking.jsx
+++ app/javascript/bundles/Bookings/components/Booking.jsx
@@ -14,7 +14,7 @@ export default class Booking extends React.Component {
       <div>
         <MyMapComponent
           isMarkerShown
-          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+          googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAH5gCwpe42PPIOJZFzW9Q33rTK9xJh3ko"
           loadingElement={<div style={{ height: `100%` }} />}
           containerElement={<div style={{ height: `400px` }} />}
           mapElement={<div style={{ height: `100%` }} />}
