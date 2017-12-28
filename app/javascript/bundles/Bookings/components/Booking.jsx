e699fc9f5a3511d9813c654b2abce917c9a1fa53
moseslucas <mo@moseslucas.com>
2017-12-28 12:54:16 +0800

    Added temporary selection of pickup, map changes

--- app/javascript/bundles/Bookings/components/Booking.jsx
+++ app/javascript/bundles/Bookings/components/Booking.jsx
@@ -5,11 +5,16 @@ import { withScriptjs, withGoogleMap, GoogleMap, Marker  } from "react-google-ma
 export default class Booking extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { title: this.props.title };
+    this._handlePickupChange = this._handlePickupChange.bind(this)
+    this.state = { 
+      title: this.props.title ,
+      lat: 14.6066560,
+      lng: 121.0816800
+    }
   }
 
   render() {
-    const {title} = this.state
+    const {title, lat, lng} = this.state
     return (
       <div>
         <MyMapComponent
@@ -17,12 +22,29 @@ export default class Booking extends React.Component {
           googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAH5gCwpe42PPIOJZFzW9Q33rTK9xJh3ko"
           loadingElement={<div style={{ height: `100%` }} />}
           containerElement={<div style={{ height: `400px` }} />}
+          lat={lat}
+          lng={lng}
           mapElement={<div style={{ height: `100%` }} />}
         />
-        <label> Pick-Up </label><input className='form-control'/>
+        <label> Pick-Up </label>
+        <select className='form-control' onChange={this._handlePickupChange}>
+          <option value='14.6066560,121.0816800'>Proud Cloud</option>
+          <option value='14.6008212,121.08805940000002'>Launch Garage</option>
+          <option value='14.601748,121.049892'> Green Hills </option>
+          <option value='10.412129,123.98614900000007'> Liloan Cebu </option>
+        </select>
         <label> Destination </label><input className='form-control'/>
       </div>
     );
+  }
+
+  _handlePickupChange (e) {
+    const pickup = e.target.value.split(',')
+    this.setState({
+      ...this.state,
+      lat: parseFloat(pickup[0]),
+      lng: parseFloat(pickup[1])
+    })
   }
 }
 
@@ -30,8 +52,8 @@ export default class Booking extends React.Component {
 const MyMapComponent = withScriptjs(withGoogleMap((props) =>
   <GoogleMap
     defaultZoom={15}
-    defaultCenter={{ lat: 14.6066560, lng: 121.0816800 }}
+    center={{ lat: props.lat, lng: props.lng }}
   >
-    {props.isMarkerShown && <Marker position={{ lat: 14.6066560, lng: 121.0816800 }} />}
+    {props.isMarkerShown && <Marker position={{ lat: props.lat, lng: props.lng }} />}
   </GoogleMap>
 ))
